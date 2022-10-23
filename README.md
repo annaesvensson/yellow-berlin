22e49d2a84c4a0b1ce685b5a266c8debb54e40c1
markseu <mark2011@mayberg.se>
2022-10-23 23:42:45 +0200

    Updated API, new way of loading translations

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <p align="right"><a href="README-de.md">Deutsch</a> &nbsp; <a href="README.md">English</a> &nbsp; <a href="README-sv.md">Svenska</a></p>
 
-# Berlin 0.8.13
+# Berlin 0.8.14
 
 Berlin is a theme inspired by Dieter Rams.
 
--- berlin.php
+++ berlin.php
@@ -2,7 +2,7 @@
 // Berlin extension, https://github.com/annaesvensson/yellow-berlin
 
 class YellowBerlin {
-    const VERSION = "0.8.13";
+    const VERSION = "0.8.14";
     public $yellow;         // access to API
     
     // Handle initialisation
@@ -16,8 +16,7 @@ class YellowBerlin {
         if ($action=="install") {
             $this->yellow->system->save($fileName, array("theme" => "berlin"));
         } elseif ($action=="uninstall" && $this->yellow->system->get("theme")=="berlin") {
-            $theme = reset(array_diff($this->yellow->system->getValues("theme"), array("berlin")));
-            $this->yellow->system->save($fileName, array("theme" => $theme));
+            $this->yellow->system->save($fileName, array("theme" => $this->yellow->system->getDifferent("theme")));
         }
     }
 }
