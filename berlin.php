9a5738529390a80a1bbdcab59409f4936360582c
markseu <mark2011@mayberg.se>
2024-04-05 00:41:18 +0200

    Updated extension

--- berlin.php
+++ berlin.php
@@ -2,7 +2,7 @@
 // Berlin extension, https://github.com/annaesvensson/yellow-berlin
 
 class YellowBerlin {
-    const VERSION = "0.8.14";
+    const VERSION = "0.9.1";
     public $yellow;         // access to API
     
     // Handle initialisation
