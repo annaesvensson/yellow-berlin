26df1ab495abd42f767384417b302430cface699
markseu <mark2011@mayberg.se>
2025-09-15 20:33:31 +0200

    Renamed Markdown block elements

--- berlin.php
+++ berlin.php
@@ -2,7 +2,7 @@
 // Berlin extension, https://github.com/annaesvensson/yellow-berlin
 
 class YellowBerlin {
-    const VERSION = "0.9.2";
+    const VERSION = "0.9.3";
     public $yellow;         // access to API
     
     // Handle initialisation
